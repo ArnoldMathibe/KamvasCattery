b23f7e8fd9a0498a1861619f8e1c5338904e9654
Arnold Kgaladi <arnoldtiisetso3@gmail.com>
2022-04-22 16:17:02 +0200

    Simnandi

--- bookings_cost/test/bookings.tests.js
+++ bookings_cost/test/bookings.tests.js
@@ -1,1 +1,5 @@
-
+describe('Bookings', function(){
+    it('This booking should cost: 135', function(Monday){
+        assert.equal(135, bookingCost(3,45));
+    });
+});
--- weekly_wages/weekly_wages.js
+++ weekly_wages/weekly_wages.js
@@ -1,3 +1,21 @@
 function weeklyWage(band, weeklyHours) {
+    var A = 23;
+    var B = 37;
+    var C = 51;
+    var total;
 
+    if(band == A)
+    {
+        for (let i = 0; i < weeklyHours.length; i++) {
+            return weeklyHours += (A*weeklyHours)
+        }
+    }else if(band == B)
+    {
+        return total += (B*weeklyHours)
+    }else if(band == B && weeklyHours.length() < 7)
+    {
+        return 'missing day'
+    }else if(band != B){
+        return 0;
+    }
 };
